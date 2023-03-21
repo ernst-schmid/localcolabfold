3ca4646e9caef7789b6a75a79cb32ab3b2bf9e50
ernst-schmid <114686630+ernst-schmid@users.noreply.github.com>
2023-03-21 19:21:28 -0400

    Update install_pipeline_linux.sh

--- install_pipeline_linux.sh
+++ install_pipeline_linux.sh
@@ -81,7 +81,7 @@ cd ~/bin
 wget https://github.com/dropbox/dbxcli/releases/download/v3.0.0/dbxcli-linux-amd64
 mv dbxcli-linux-amd64 dbxcli
 chmod +x dbxcli
-~/bindbxcli account
+~/bin/dbxcli account
 
 
 
