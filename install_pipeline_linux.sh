15e74ab897830a413c0a0435959531445db7afa1
ernst-schmid <114686630+ernst-schmid@users.noreply.github.com>
2023-03-24 16:34:26 -0400

    Update install_pipeline_linux.sh

--- install_pipeline_linux.sh
+++ install_pipeline_linux.sh
@@ -57,7 +57,7 @@ export TF_FORCE_UNIFIED_MEMORY="1"
 export CUDA_VISIBLE_DEVICES=\$1
 export XLA_PYTHON_CLIENT_MEM_FRACTION="8.0"
 export COLABFOLDPATH=${COLABFOLDDIR}
-export PATH="\$COLABFOLDPATH/colabfold-conda/bin:$PATH"
+export PATH="\$COLABFOLDPATH/colabfold-conda/bin:\$PATH"
 \$COLABFOLDPATH/colabfold-conda/bin/colabfold_batch "\${@:2}" & echo \$! > ${CURRENTPATH}/colabfold_gpu_\$1'_pid.txt'
 EOF
 chmod +x ./colabfold_batch
