b8b6722132d26704445badffa4045727897d55f4
ernst-schmid <114686630+ernst-schmid@users.noreply.github.com>
2023-04-11 09:31:34 -0400

    Update download_from_dbx.sh

--- download_from_dbx.sh
+++ download_from_dbx.sh
@@ -1,22 +1,46 @@
 #!/bin/bash
 
-folder_name=$1;
-dest_name=$2;
-pattern=$3;
-
-files=$(~/bin/dbxcli ls "$folder_name")
-matching_files=()
-
-for f in $files
-do
-    filename=$(basename $f)
-    if [[ "$filename" == $pattern ]]; then
-         matching_files+=("$f")        
-    fi
+filenames_to_keep=''
+while getopts "f:" opt; do
+  case $opt in
+    f)
+      filenames_to_keep=$OPTARG
+      echo "Argument supplied: $filenames_to_keep";;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
 done
 
-cd $dest_name
+shift $((OPTIND -1))
 
-for f in "${matching_files[@]}"; do
-  ~/bin/dbxcli get $f
+dbx_folder_name=$1
+dest_name=$2
+pattern=$3
+
+dbx_files=$(dbxcli ls "$dbx_folder_name" | tr -s [:space:] "\n")
+matching_dbx_files=()
+
+if [ -n "$filenames_to_keep" ]; then
+
+    while read -r line; do
+        line="${line%"${line##*[![:space:]]}"}"
+        match=$(echo "$dbx_files" | grep "$line")
+        if [ -n "$match" ]; then
+            readarray files <<< "$match"
+            matching_dbx_files+=("${files[@]}")
+        fi
+    done < $filenames_to_keep
+else
+    readarray files <<< "$dbx_files"
+    matching_dbx_files=("${files[@]}")
+fi
+
+cd $dest_name
+for f in "${matching_dbx_files[@]}"; do
+
+    filename=$(basename $f)
+    if [[ "$filename" == $pattern ]]; then
+        ~/bin/dbxcli get $f
+    fi
 done
