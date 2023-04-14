a7a6472a9d80e0d923d3bac691675d65942467de
ernst-schmid <114686630+ernst-schmid@users.noreply.github.com>
2023-04-13 23:59:31 -0400

    Update upload_to_dbx.sh

--- upload_to_dbx.sh
+++ upload_to_dbx.sh
@@ -3,15 +3,16 @@ folder_name=$1;
 dest_name=$2;
 pattern=$3;
 
-existing_files=$(~/bin/dbxcli ls "$dest_name")
+existing_files=$(~/bin/dbxcli ls "$dest_name" | tr -s [:space:] "\n")
 
 for file in $(find $folder_name -name "$pattern")
 do
+  echo $file
   parent_folder=$(dirname "$file")
   immediate_parent=$(basename "$parent_folder")
   filename="$(basename $file)"
   destination="$dest_name/$filename"
-  if echo "$existing_files" | grep -qFx "$destination"; then
+  if echo "$existing_files" | grep -qF "$destination"; then
     echo "skipping"
   else
      ~/bin/dbxcli put -v $file $destination
